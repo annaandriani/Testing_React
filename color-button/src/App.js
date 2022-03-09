0a11d7342cb3750d891b108ede4d9c2d0047e325
Anna Andriani <80374093+annaandriani@users.noreply.github.com>
2022-03-09 02:54:38 +0000

    "Test and code function to transform camel case colors to spaced colors"

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import './App.css';
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+}
+
 
 function App() {
   const  [ buttonColor, setButtonColor ] = useState('red');
@@ -9,15 +13,23 @@ function App() {
   
   return (
     <div>
-      <button style ={{backgroundColor:buttonColor}} onClick={() => setButtonColor (newButtonColor)} disabled={disabled}>Change to {newButtonColor} </button>
-  <input type="checkbox"
-  id="enable-button-checkbox"
-  defaultChecked={disabled}
-    aria-checked={disabled}
-      onChange={(e) =>  setDisabled(e.target.checked)}/>
-<label htmlfor="disable-button-checkbox"> Disable button </label>  
-
-  </div>
-);
+      <button
+        style={{ backgroundColor: disabled ? 'gray' : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {newButtonColor}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
+    </div>
+  );
 }
 export default App;
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color', () => {
   render(<App />);          
@@ -47,5 +48,51 @@ expect(colorButton).toBeDisabled();
 fireEvent.click(checkbox);
 expect(colorButton).toBeEnabled();
 });
-  
-  
+
+
+test('Disabled button has gray background and reverts to red', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button', });
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  // disable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle('background-color: gray');
+
+  // re-enable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle('background-color: red');
+});
+
+test('Clicked disabled button has gray background and reverts to blue', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  // change button to blue
+  fireEvent.click(colorButton);
+
+  // disable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle('background-color: gray');
+
+  // re-enable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle('background-color: blue');
+});
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
+});
