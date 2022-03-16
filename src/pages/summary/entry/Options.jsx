23f94608340e01697324e828e9d081f9ac0d4673
annaandriani <anninhaandriani@gmail.com>
2022-03-15 21:45:46 -0300

    add toppings

--- src/pages/summary/entry/Options.jsx
+++ src/pages/summary/entry/Options.jsx
@@ -1,29 +1,31 @@
-import Row from 'react-bootstrap/Row';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOptions';
+import ToppingOption from './ToppingOption';
 
-export default function Options({optionType}){
-    const [items, setItens] = useState([]);
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
 
-    useEffect(()=> {
-        axios
-        .get('http://localhost:3030/${optionType}')
-        .then((response) => setItens(response.data))
-        .catch((error) => {
-            //TODO: (handle error response)
-        });
-    }, [optionType]);
-    const ItemComponent =  optionType == 'scoops' ? ScoopOption : null;
-    
-    const optionsItems = items.map(item=> 
-    <ItemComponent 
-        key={item.name}
-        name={item.name}
-        imagePath={item.imagePath}
-         
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
     />
-    );
+  ));
 
-    return <Row>(optionItems);</Row>
+  return <Row>{optionItems}</Row>;
 }
